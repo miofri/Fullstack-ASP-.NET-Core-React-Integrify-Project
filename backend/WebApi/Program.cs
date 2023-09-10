ed47502ac1b9cbcda1b952b603a2a5f350aed700
Selvi <slvkesuma@gmail.com>
2023-09-10 22:53:50 +0300

    changed compromised psql password

--- backend/WebApi/Program.cs
+++ backend/WebApi/Program.cs
@@ -18,7 +18,9 @@ using WebApiDomain.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 var timeStampInterceptor = new TimeStampInterceptor();
-var npgsqlBuilder = new NpgsqlDataSourceBuilder(builder.Configuration.GetConnectionString("Azure"));
+var npgsqlBuilder = new NpgsqlDataSourceBuilder(
+    builder.Configuration.GetConnectionString("Azure")
+);
 npgsqlBuilder.MapEnum<Role>();
 npgsqlBuilder.MapEnum<OrderStatus>();
 var dataSource = npgsqlBuilder.Build();
